74ad28b5c1fd05d29e9404915d465262bdcf0818
Domingo Dirutigliano <dirutigliano20@gmail.com>
2020-09-07 20:00:23 +0200

    Add Custom Timeout and added some comments around the python files

--- start.py
+++ start.py
@@ -5,10 +5,13 @@ import utils.config, utils.fun
 from time import sleep
 
 
-#Creating the logger
+#Creating the loggers
 
 log = utils.fun.setup_logger('CTFsub',utils.config.GLOBAL_LOG_FILE)
 flag_log = utils.fun.setup_logger('flags',utils.config.GLOBAL_FLAG_FILE,'%(asctime)s FLAG: %(message)s')
+
+
+#This comment is an idea for the shell
 '''
 {
 
@@ -31,23 +34,27 @@ config:{
 }
 }
 '''
+
+#global vars synced with relative files
 class glob: 
     constant_vars = {}
     commit_lock = threading.Lock()
     settings = {}
     settings_lock = threading.Lock()
 
+#Save changes in constant_vars in file g_var.json
 def commit_vars_changes():
     glob.commit_lock.acquire()
     open(utils.config.GLOBAL_DATA_FILE,'wt').write(json.dumps(glob.constant_vars))
     glob.commit_lock.release()
 
+#Save changes in settings in file settings.json
 def commit_settings():
     glob.settings_lock.acquire()
     open(utils.config.GLOBAL_SETTINGS_FILE,'wt').write(json.dumps(glob.settings))
     glob.settings_lock.release()
 
-#Load Glob vars
+#Load Glob vars (charging it from file (to get back old status))
 fl = open(utils.config.GLOBAL_DATA_FILE,'rt').read()
 if fl != "":
     glob.constant_vars = json.loads(fl)
@@ -56,6 +63,7 @@ fl = open(utils.config.GLOBAL_SETTINGS_FILE,'rt').read()
 if fl != "":
     glob.settings = json.loads(fl)  
 
+#Function used for submit a flag
 def submit_flag(flags:list):
     flags = re.findall(utils.config.FLAG_REGEX," ".join(flags)) #Filetring all recived flags
     sended = []
@@ -71,9 +79,11 @@ def submit_flag(flags:list):
                 flag_log.exception(e)
     del sended
 
+#Get python module file of the attack and use it
 def get_attack_by_name(attack_name):
     return getattr(__import__(f"{utils.config.ATTACK_PKG}.{attack_name}"),attack_name)
 
+#Thread function that have to execute a function
 def start_attack(py_attack,assigned_ip):
     #Starting the attack...
     try:
@@ -87,30 +97,37 @@ def start_attack(py_attack,assigned_ip):
         #Get the flag and submit
         flags_obtained = 'service_closed'
 
+        #Get attack settings
         attack_settings = glob.settings['process_controller'][py_attack]
 
+        #Managing alive control for the remote service
         if not (attack_settings['alive_ctrl'] is None):
+            #if wanted as decribed in the attack file, we control first if the service is on
             if not utils.fun.is_port_in_use(assigned_ip,attack_settings['alive_ctrl']):
                 log.warning(f'We found closed port attacking {assigned_ip}:{attack_settings["alive_ctrl"]} with {py_attack} attack')
                 return
 
+        #Queue for get the result from Process used for put a TIMEOUT at the attack
         res = multiprocessing.Queue()
 
         var_dict = {}
         
+        #Set up structure for save permanent vars
         if this_attack.attack_name not in glob.constant_vars.keys():
             glob.constant_vars[this_attack.attack_name] = {}
             commit_vars_changes()
-            
         if assigned_ip in glob.constant_vars[this_attack.attack_name].keys():
             var_dict = glob.constant_vars[this_attack.attack_name][assigned_ip]
         
+        # choose what timeout time to use
+        timeout_process = utils.config.TIMEOUT_ATTACK
+        if not attack_settings['timeout'] is None:
+            timeout_process = attack_settings['timeout']
 
+        #Start the process
         p = multiprocessing.Process(target=this_attack.get_flags,args=(assigned_ip, var_dict, res))
         p.start()
-
-        # Wait for TIMEOUT TIME
-        p.join(utils.config.TIMEOUT_ATTACK)
+        p.join(timeout_process)
 
         # If thread is still active we kill it
         if p.is_alive():
@@ -118,18 +135,22 @@ def start_attack(py_attack,assigned_ip):
             p.terminate()
             p.join()
         else:
+            #get the result (sending the flag and saving changes of permanent vars)
             flags_obtained = res.get()
             glob.constant_vars[this_attack.attack_name][assigned_ip] = res.get()
             commit_vars_changes()
         
         del this_attack
         
+        #Updating the status of auto blacklist
         blacklist_status = glob.settings['blacklist'][py_attack][assigned_ip]
 
         if flags_obtained is None or flags_obtained == 'leak_closed':
+            #If this attack is a try to re-attack a team that probably have closed the vuln
             if blacklist_status['stopped_times'] == -1 and blacklist_status['excluded']:
                 blacklist_status['stopped_times'] = 0
             else:
+                #Count fail time, if it fails enougth times, we put it in the auto blacklist
                 blacklist_status['fail_times'] += 1
                 if blacklist_status['fail_times'] >= utils.config.TIMES_TO_BLACKLIST:
                     #Start auto blacklist
@@ -138,14 +159,15 @@ def start_attack(py_attack,assigned_ip):
 
             commit_settings()
 
-
-        if flags_obtained is None:
+        #Send a different message on log for different situations
+        if flags_obtained is None: # Unexpected error
             log.error(f'unexpected error attacking {assigned_ip} using {py_attack} attack have been raised')
         elif flags_obtained == 'leak_closed': #Attack failed (Because the leak is closed)
             log.warning(f'Attack to {assigned_ip} using {py_attack} attack FAILED (leak closed)!')
-        elif flags_obtained == 'service_closed':
+        elif flags_obtained == 'service_closed': # Service not reponding or take lot's of time
             log.warning(f'Find closed service to {assigned_ip} using {py_attack} attack !')
         else:
+            # if the attack went successfully send the flag and reset blacklist status (Usefull for an attack reopened)
             blacklist_status = {
                     'excluded':False,
                     'fail_times':0,
@@ -163,11 +185,13 @@ def main():
     log.info('CTFsub is starting !')
     while True:
         
+        #Get start time for calculating at the end of the attack how many time have to wait
         last_time = time.time()
 
+        #Wait for find python attack files
         wait_for_attacks = False
-        while True:
-
+        while True:  
+            # Get list of python attack files
             to_exec = utils.fun.get_pythonfile_list(utils.config.ATTACKS_FOLDER) #Taking the list of the python executable to run for the attack
             if len(to_exec) == 0 or to_exec == ['__init__']:
                 if wait_for_attacks == False:
@@ -178,7 +202,8 @@ def main():
                 if wait_for_attacks == True:
                     log.info('Python attack file found! STARTING...')
                 break
-
+        
+        # Start the round
         flag_log.info('ROUND STARTED')
         log.info('Round Started')
         thread_list = []
@@ -193,12 +218,11 @@ def main():
                 if team_id == utils.config.OUR_TEAM_ID: continue # Skiping our team
                 ip_to_attack = utils.fun.get_ip_from_temp(utils.config.IP_VM_TEMP,{'team_id':team_id}) #calculating the ip
                 
-                
+                #Wait for a free thread following the THREADING_LIMIT
                 while len(thread_list) >= utils.config.THREADING_LIMIT:
                     #Wait for a free thread
                     for i in range(len(thread_list)):
                         if not thread_list[i].is_alive():
-                            print('odhjiiojsda')
                             thr = thread_list.pop(i); del thr
                             break
                     time.sleep(.1)
@@ -228,8 +252,10 @@ def main():
                         'on':True,
                         'whitelist_on':False,
                         'excluded_ip':[],
-                        'whitelist_ip':[]
+                        'whitelist_ip':[],
+                        'timeout':None
                     }
+                    # Try to charge all specific configs from the attack file
                     try:
                         atk = get_attack_by_name(py_f)
                         if 'CONFIG' in dir(atk):
@@ -241,24 +267,28 @@ def main():
                         log.error('ERROR LOADING CONFIG FROM ATTACK')
                         log.exception(e)
                 
+
                 attack_blacklist = glob.settings['blacklist'][py_f][ip_to_attack]
                 attack_settings = glob.settings['process_controller'][py_f]
                 
                 commit_settings()
 
-                if attack_settings['on']:
+                #Choose what action do following the istruction in settings dict
+                if attack_settings['on']: # If the attack is enabled
                     #Control list controls
-                    if attack_settings['whitelist_on']:
+                    if attack_settings['whitelist_on']: #Filtering with blacklist or withlist
+                        #Filter whitelist
                         if ip_to_attack not in attack_settings['whitelist_ip']:
                             continue
                     else:
+                        #Filter blacklist
                         if ip_to_attack in attack_settings['excluded_ip']:
                             log.warning(f'{py_f} attack on IP {ip_to_attack} in blacklist... skipping')
                             continue
                     #Auto blacklist control
                     if attack_blacklist['excluded']:
                         if attack_blacklist['stopped_times'] == -1:
-                            pass # Last time the service was closed so we have to try again
+                            pass # Last time the service was closed so we have to try again to attack
                         if attack_blacklist['stopped_times'] >= utils.config.TIME_TO_WAIT_IN_BLACKLIST:
                             log.warning(f'auto_blacklist enabled on ip {ip_to_attack} of {py_f} attack ... trying anyway to execute')
                             attack_blacklist['stopped_times'] = -1 # If success -1 remember to reset blacklist
@@ -272,9 +302,7 @@ def main():
                     log.warning(f'{py_f} attack disabilited! Skipping...')
                     break
                 
-                
-
-                #Starting new thread
+                #Starting new thread for attack
                 log.info(f'Starting thread {py_f} attack for {ip_to_attack}')
                 thread_list.append(threading.Thread(target=start_attack,args=(py_f,ip_to_attack)))
                 thread_list[-1].daemon = True
@@ -282,7 +310,7 @@ def main():
 
             log.info(f'{py_f} attack, finished!')
             
-        #Wait for finish
+        #Wait for threads work finish
         log.info('Waiting for thread work finish')
         while len(thread_list) != 0:
             #Wait for a free thread
@@ -294,6 +322,7 @@ def main():
         del thread_list
         log.info(f'Attacks for this round finished')
 
+        #Wait remaing time
         this_time = time.time()
         time_to_wait = utils.config.TICK_TIME - (this_time - last_time)
         if time_to_wait > 0:
